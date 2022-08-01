6056a3229d1337fb0879b52268f5c24a712ef3b9
Jon-Cerr <109481604+Jon-Cerr@users.noreply.github.com>
2022-08-01 15:47:08 -0500

    Add files via upload
    Initial commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.onscroll = function () {
+
+    scroll = document.documentElement.scrollTop;
+
+    header = document.getElementById("header");
+
+    if (scroll > 20) {
+        header.classList.add('nav_mod');
+    } else if (scroll < 20) {
+        header.classList.remove('nav_mod');
+    }
+
+}
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+menu = document.getElementById("header");
+body = document.getElementById("content_all");
+nav = document.getElementById("nav");
+footer = document.getElementById("container_footer");
+section = document.getElementById("skills");
+
+function mostrar_menu() {
+
+    menu.classList.toggle('move_content');
+    body.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+    footer.classList.toggle('move_content');
+    section.classList.toggle('move_content');
+}
+
+
+window.addEventListener("resize", function () {
+
+    if (window.innerWidth > 760) {
+        menu.classList.remove('move_content');
+        body.classList.remove('move_content');
+        nav.classList.remove('move_nav');
+        footer.classList.remove('move_content');
+        section.classList.remove('move_content');
+    }
+
+});
